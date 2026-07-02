1daa03786f8d8032deefb700ba0809fad496170a
g3org3 <22123gt#hvhs.school.nz>
2026-07-02 22:29:57 +1200

    hotfix5

--- games/gtn/gtn.mjs
+++ games/gtn/gtn.mjs
@@ -149,6 +149,11 @@ async function guessNumber(result){
         score = 0;
     }
 
+    if (NUM == "") {
+        feedBackLine.innerHTML = "Invalid number";
+        return;
+    }
+    
     if (NUM <= -1 || NUM >= 101){
         feedBackLine.innerHTML = "Invalid number, must be between 0 and 100";
         return;
